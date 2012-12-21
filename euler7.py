51cf2846d7a1b90364df81e9a39f2aa2ca989303
Georgia <georgia.reh@gmail.com>
2012-12-21 12:10:56 -0800

    slightly more efficient

--- euler7.py
+++ euler7.py
@@ -3,24 +3,24 @@ from math import sqrt
 def nextprime(number): 
 	"This function will find the smallest prime larger than the current number"
 	potential= number
-	j=2
-	while j<potential:
+	j=3
+	while j<=sqrt(potential):
 	        if potential%j:
-			j=j+1
+			#print "j", j #for debugging
+			j=j+2 #note: it is trying to divide potential by all odd numbers but necessarily primes, so its catching all the non primes as quickly as possible but taking longer than necessary on the primes
 		else:
-			#print "not prime"
-			potential=potential +1
-			j=2
+			#print "j", j #for debugging
+			#print potential, "not prime" #for debugging  
+			potential=potential +2 #skips even numbers  
+			j=3
 			continue
-	#print potential
+	#print potential #for debugging 
 	return potential 
 print ("project euler problem 7: what is the 10001st prime number?")
-number =2
-for i in range (10001):
+number =3
+for i in range (1, 10001):
 	number = nextprime(number) 
-	#print (number)
-	number = number +1
+	print (number)
+	number = number +2
 	continue 
 #number = nextprime(number)	
-number = number -1 
-print number
