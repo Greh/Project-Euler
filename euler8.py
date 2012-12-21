76f90f90c8c7a59dd50afa6a97a7553cd19d442a
Georgia <georgia.reh@gmail.com>
2012-12-21 09:38:55 -0800

    added project euler problem 8

---
+++ euler8.py
@@ -0,0 +1,6 @@
+print "project euler problem 8: find the greatest product of five consecutive digits in this 1000 digit number"
+import array 
+number=open("/home/georgia/problem8.txt","r")
+#print number.readline()
+print 9*8*7*9*9
+
