124fb3d690d8879e395b7e402c61f5d9a2450eaa
Georgia <georgia.reh@gmail.com>
2013-02-21 18:55:32 -0800

    I used a sieve, its twice as fast now

--- euler10.py
+++ euler10.py
@@ -1,31 +1,22 @@
-#this program is not ideally suited to change the divisor variable into an array of indexed primes, use nextprime.py instead
 print "find the sum of all prime numbers below two million"
-#!/usr/bin/env python 3.1
-from math import sqrt 
-def nextprime(number): 
-	"This function will find the smallest prime larger than the current number"
-	potential= number
-	j=3
-	while j<=sqrt(potential):
-	        if potential%j:
-			#print "j", j #for debugging
-			j=j+2 #note: it is trying to divide potential by all odd numbers but necessarily primes, so its catching all the non primes as quickly as possible but taking longer than necessary on the primes
-		else:
-			#print "j", j #for debugging
-			#print potential, "not prime" #for debugging  
-			potential=potential +2 #skips even numbers  
-			j=3
-			continue
-	#print potential #for debugging 
-	return potential 
-number =3 #this number must stay three, do not change or all the +2 counters will skip odd numbers and check even numbers 
-summation = 2 
-stop = 2000000
-while number < stop:
-	number = nextprime(number) 
-	if number<stop: 
-		print number 
-		summation = summation + number 
-	number= number +2 #skips even numbers 
-	continue 
-print summation 
+
+total=0 
+print "seive of eratosthenes"
+end=raw_input("find all the primes below... ") 
+bucket = [];
+for i in range(0,int(end)):
+    bucket.append(0);
+#print bucket
+j=2
+while j**2<=int(end): 
+	for i in range(j+j, len(bucket), j): 
+		bucket[i]=1
+	#print bucket
+	j=j+1
+bucket[0]=1
+bucket[1]=1
+for i in range(len(bucket)):
+	if bucket[i]==0:
+		total=total+i 
+		print total 
+	
